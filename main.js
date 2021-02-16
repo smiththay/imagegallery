91aac6143035acf20c44b610312bf27d1356b7a3
Smith Thay <S.T.@Kathryns-MacBook-Pro.local>
2021-02-16 17:53:01 -0500

    functional code

--- main.js
+++ main.js
@@ -6,8 +6,26 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+for(let i = 1; i < 6; i++) {
+    const pic = ('images/pic' + i +'.jpg');
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', pic);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function() {
+        displayedImage.src = pic;
+    }
+}
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function(){
+if(btnClass === 'dark'){
+btn.setAttribute('class', 'light');
+btn.textContent = "lighten";
+overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+} else {
+    (btnClass === 'light')
+    btn.setAttribute('class', 'dark');
+    btn.textContent = "darken";
+    overlay.style.background = "rgba(0,0,0,0)";
+}
+}
